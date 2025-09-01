0a6ddd96551d5b95fb9ae48b48b71c6eb543ec7c
Gary Yamashita <garyjyam@gmail.com>
2025-09-01 16:40:15 -0700

    update variables

--- src/app/variables.ts
+++ src/app/variables.ts
@@ -1,3 +1,11 @@
 export const node_env = import.meta.env.VITE_NODE_ENV;
 
 export const reddit_url = import.meta.env.VITE_REDDIT_URL;
+
+/*
+
+VITE_NODE_ENV=development
+
+VITE_REDDIT_URL=https://www.reddit.com
+
+*/
